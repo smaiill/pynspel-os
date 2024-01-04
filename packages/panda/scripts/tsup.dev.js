eebbae4f7a375aa0ee17566804ef4f34bbcc7f33
niiyy <smailaberkaoui@gmail.com>
2024-01-04 22:28:08 +0100

    fix: fixed issue going with panda package

--- packages/panda/scripts/tsup.dev.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  entry: ['src/index.ts'],
-  watch: true,
-  dts: true,
-  format: ['cjs', 'esm'],
-  external: '@pandacss/dev"',
-})
